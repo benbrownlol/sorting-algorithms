739d7035c716f9bb782a3505d1469a958c941d0b
Ben Brown <ben@ben-brown.co>
2016-11-14 21:30:38 +0000

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,18 +2,18 @@ const example = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 
 function bubbleSort(input) {
   const array = [...input];
-  let swapped = true;
-  while (swapped) {
-    swapped = false;
+  let swapping = true;
+  while (swapping) {
+    swapping = false;
     for (let i = 0; i < array.length; i++) {
       if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
         [array[i], array[i + 1]] = [array[i + 1], array[i]];
-        swapped = true;
+        swapping = true;
       }
     }
   }
   return array
 }
 
-const sorted = bubbleSort(example);
-console.log(sorted);
+const output = bubbleSort(example);
+console.log(output);
