63825c933240e0fc56c261a9d67cc6c68f42c437
Ben Brown <ben@ben-brown.co>
2016-11-14 22:25:18 +0000

    Create merge-sort.js
    Basic Merge Sort implementation

---
+++ merge-sort.js
@@ -0,0 +1,31 @@
+const example = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+function mergeSort(input, method) {
+  const array = [...input];
+
+  if (array.length < 2) return array;
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return sort(mergeSort(left, 'Top Down'), mergeSort(right, 'Top Down'));
+
+  function sort(left, right) {
+    const sorted = [];
+
+    while (left.length && right.length) {
+      if (left[0] < right[0]) sorted.push(left.shift());
+      else sorted.push(right.shift());
+    }
+
+    return sorted
+      .concat(...left)
+      .concat(...right);
+  }
+  
+  return array;
+}
+
+const output = mergeSort(example, 'Top Down');
+console.log(output);
