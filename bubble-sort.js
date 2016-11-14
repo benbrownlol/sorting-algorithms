41732056568623b62d0a3b32a0b8ae8729282f27
Ben Brown <ben@ben-brown.co>
2016-11-14 23:05:06 +0000

    Update bubble-sort.js

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,20 @@
-const example = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const example = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]; // example data
 
 function bubbleSort(input) {
-  const array = [...input];
-  let swapping = true;
+  const array = [...input]; // clone input array for Pure Function
+
+  let swapping = true; // init swapping variable to start `while` loop
   while (swapping) {
-    swapping = false;
+    swapping = false; // stop `while` loop if conditional not met
     for (let i = 0; i < array.length; i++) {
       if (array[i] && array[i + 1] && array[i] > array[i + 1]) {
-        [array[i], array[i + 1]] = [array[i + 1], array[i]];
-        swapping = true;
+        [array[i], array[i + 1]] = [array[i + 1], array[i]]; // swap array values
+        swapping = true; // keep `while` loop running
       }
     }
   }
-  return array
+
+  return array // return cloned array
 }
 
 const output = bubbleSort(example);
