19efb5a9a0d25dba487a4e851eb870ee0997cc44
Ben Brown <ben@ben-brown.co>
2016-11-14 23:00:32 +0000

    Update merge-sort.js

--- merge-sort.js
+++ merge-sort.js
@@ -1,22 +1,22 @@
-const example = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const example = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8]; // example data
 
 function mergeSort(input) {
-  const array = [...input];
+  const array = [...input]; // clone input array for Pure Function
+  
+  if (array.length < 2) return array; // array small enough, just return
 
-  if (array.length < 2) return array;
+  const middle = Math.floor(array.length / 2); // get middle index of array
+  const left = array.slice(0, middle); // split array and store left half
+  const right = array.slice(middle); // split array and store right half
 
-  const middle = Math.floor(array.length / 2);
-  const left = array.slice(0, middle);
-  const right = array.slice(middle);
-
-  return sort(mergeSort(left), mergeSort(right));
+  return sort(mergeSort(left), mergeSort(right)); // recursively split & sort array 
 
   function sort(left, right) {
-    const sorted = [];
+    const sorted = []; // sorted array
 
-    while (left.length && right.length) {
-      if (left[0] < right[0]) sorted.push(left.shift());
-      else sorted.push(right.shift());
+    while (left.length && right.length) { // keep shifting array values until both passed arrays are empty
+      if (left[0] < right[0]) sorted.push(left.shift()); // left is smaller, add value to sorted array
+      else sorted.push(right.shift()); // right is smaller, add value to sorted array
     }
 
     return sorted
@@ -24,7 +24,7 @@ function mergeSort(input) {
       .concat(...right);
   }
   
-  return array;
+  return array; // return cloned array
 }
 
 const output = mergeSort(example);
